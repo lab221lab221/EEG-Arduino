a235a4985ee5287c85d0e2b1bb8f3efe15c7b3c1
lab221lab221 <36805337+lab221lab221@users.noreply.github.com>
2022-05-14 14:09:41 -0400

    Update EEG.ino

--- EEG.ino
+++ EEG.ino
@@ -1,1 +1,22 @@
+const int holdButtonPin = 3;
+const int inputButtonPin = 4;
 
+int counter = 0;
+void setup()
+{
+  pinMode(buttonPin, INPUT);
+  pinMode(redPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(bluePin, OUTPUT);
+}
+
+
+void loop()
+{
+  int buttonState;
+  buttonState = digitalRead(buttonPin);
+  
+  if (buttonState==HIGH) {
+  
+  }
+}
